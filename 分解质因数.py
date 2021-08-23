00a6179dc1fd5a281175ee4ad37d2531ab2bf532
Jack-Feb <76858585+Jack-Feb@users.noreply.github.com>
2021-08-23 15:06:03 +0800

    Update 分解质因数.py

--- 分解质因数.py
+++ 分解质因数.py
@@ -12,18 +12,3 @@ def findprime(a):
                 break     
     list_all = list_ + _list
     # print(list_all)
-def res():  
-    def findprime(a):
-        list_ = []
-        _list = []
-        while a>1:
-            for i in range(2,a+1):
-                if a % i == 0:
-                    a = int(a/i)
-                    if a == 1:
-                        list_.append(i)
-                    else:
-                        _list.append(i)
-                    break     
-        list_all = list_ + _list
-        # print(list_all)
