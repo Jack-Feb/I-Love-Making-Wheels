85a52f82178a2fab29c244329a1296c50af88ff5
Jack-Feb <76858585+Jack-Feb@users.noreply.github.com>
2021-08-24 16:43:34 +0800

    Update 数据处理函数.py

--- 数据处理函数.py
+++ 数据处理函数.py
@@ -3,3 +3,6 @@ def check_missing_data(df):
     return df.isnull().sum().sort_values(ascending=False)
 
 def convert_cat2num(df):
+    num_encode = {'col_1' : {'YES':1, 'NO':0},
+                  'col_2'  : {'WON':1, 'LOSE':0, 'DRAW':0}}  
+    df.replace(num_encode, inplace=True)
