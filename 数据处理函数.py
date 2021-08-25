b27c7be7d854cc29c4a94da0dc67e449e2af98b4
Jack-Feb <76858585+Jack-Feb@users.noreply.github.com>
2021-08-25 19:54:49 +0800

    Update 数据处理函数.py

--- 数据处理函数.py
+++ 数据处理函数.py
@@ -7,10 +7,9 @@ def convert_cat2num(df):
     num_encode = {'col_1' : {'YES':1, 'NO':0},
                   'col_2'  : {'WON':1, 'LOSE':0, 'DRAW':0}}  
     df.replace(num_encode, inplace=True)
-    
+   
+# 将最后三个字符为aaa的字符串拼接
 def concat_col_str_condition(df):
-    # concat 2 columns with strings if the last 3 letters of the first column are 'pil'
-    mask = df['col_1'].str.endswith('pil', na=False)
+    mask = df['col_1'].str.endswith('aaa', na=False)
     col_new = df[mask]['col_1'] + df[mask]['col_2']
-    col_new.replace('pil', ' ', regex=True, inplace=True)  # replace the 'pil' with emtpy space
-
+    col_new.replace('aaa', ' ', regex=True, inplace=True) 
