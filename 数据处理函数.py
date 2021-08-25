b075d6982ab19f6136df43cfa3226c0ff3cd95c3
Jack-Feb <76858585+Jack-Feb@users.noreply.github.com>
2021-08-25 19:45:17 +0800

    Update 数据处理函数.py

--- 数据处理函数.py
+++ 数据处理函数.py
@@ -7,3 +7,5 @@ def convert_cat2num(df):
     num_encode = {'col_1' : {'YES':1, 'NO':0},
                   'col_2'  : {'WON':1, 'LOSE':0, 'DRAW':0}}  
     df.replace(num_encode, inplace=True)
+
+def concat_col_str_condition(df):
